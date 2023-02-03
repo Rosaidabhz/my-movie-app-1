4b0364d795c0a2aab3a409ed1b20be84ab8bd168
rosaidabhz <rosaidabhz@gmail.com>
2023-02-03 15:56:45 -0500

    servicios

--- service/genres.py
+++ service/genres.py
@@ -1,19 +1,27 @@
-from models.genres import Genres as GenresModel
-from schemas.genres  import Genres
+from models.genres import Genres as GenresModel 
 
 class GenresService():
-
-    def init (self,db) -> None:
+    def __init__(self,db) -> None:
         self.db = db
-
-    def get_genres(self):
+        
+    def get_genres (self) -> GenresModel:
         result = self.db.query(GenresModel).all()
         return result
-
-    def get_genres(self,id:int):
+    
+    def get_genres_id (self, id:int):
         result = self.db.query(GenresModel).filter(GenresModel.id == id).first()
-        return result
-
-    def get_genres(self,title:str):
-        result = self.db.query(GenresModel).filter(GenresModel.title == title).last()
-        return result
+        return result 
+    
+    def create_genres (self, genres: GenresModel):
+        new_genres = GenresModel(
+            gen_id = gen.id,
+            gen_title = gen.title
+        )
+        self.db.add(new_genres)
+        self.db.commit()
+        return
+    
+        def delete_genres(self,id:int):
+            self.db.query(GenresModel).filter(GenresModel.id == id).delete()
+            self.db.commit()
+            return
